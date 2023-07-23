a1c71ab4e375076db8416f7091ee3f9ce5e51c89
Jay Cho <jaycho1214@gmail.com>
2023-07-23 19:48:22 +0900

    set custom 404 error

--- main.cc
+++ main.cc
@@ -2,12 +2,19 @@
 #include <mysql.h>
 #include <cstdlib>
 
+void setCustom404() {
+    Json::Value result;
+    result["status"] = 404;
+    result["message"] = "Not Found";
+    drogon::app().setCustom404Page(drogon::HttpResponse::newHttpJsonResponse(result));
+}
+
 int main() {
     drogon::app().loadConfigFile("../config.json");
   
     int port = atoi(std::getenv("PORT"));
     port = port == 0 ? 1477 : port;
-
+    setCustom404();
     LOG_INFO << "Listening at 0.0.0.0:" << port;
     //Set HTTP listener address and port
     drogon::app().addListener("0.0.0.0", port);
